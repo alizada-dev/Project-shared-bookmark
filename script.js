cc5ff954046ffe2598ac3605703bb4f328986599
RomanSanaye <sanaye143@gmail.com>
2026-06-03 15:44:24 +0100

    Display user bookmarks in reverse chronological order and handle empty state with alert

--- script.js
+++ script.js
@@ -25,5 +25,50 @@ function loadUsers() {
   });
 }
 
+// select-user from dropdown event-listener;
+userSelect.addEventListener("change", (e) => {
+  currentUser = e.target.value;
+  const userData = getData(currentUser) || [];
+  if (userData.length === 0) {
+    alert("No bookmarks saved yet !");
+    bookmarkForm.style.display = "block";
+    bookmarksBox.style.display = "none";
+    return;
+  }
+
+  bookmarkForm.style.display = "block";
+  bookmarksBox.style.display = "block";
+
+  displayBookmarks();
+});
+
+// display bookmarks function;
+function displayBookmarks() {
+  const bookmarks = getData(currentUser) || [];
+  bookmarks.sort((a, b) => {
+    return new Date(b.timeStamp) - new Date(a.timeStamp);
+  });
+  bookmarksBox.innerHTML = "";
+
+  const savedBookmarkTitle = document.createElement("h2");
+  savedBookmarkTitle.textContent = "Saved Bookmarks";
+  bookmarksBox.appendChild(savedBookmarkTitle);
+
+  bookmarks.forEach((bookmark, index) => {
+    bookmarksBox.innerHTML += `
+    <div class="bookmark-details">
+    <a class="site-title" href="${bookmark.siteUrl}">${bookmark.siteName}</a>
+    <p class="site-desc">${bookmark.siteDescription}</p>
+    <h3 class="time-stamp">${bookmark.timeStamp}</h3>
+    <div class="bookmark-span">
+    <span onclick="copyToClipboard(${index})">Copy</span>
+    <span onclick="shareBookmark(${index})">Share</span>
+    <span onclick="deleteBookmark(${index})">Delete</span>
+    </div>
+    </div>
+    `;
+  });
+}
+
 // load data automatically;
 window.onload = loadUsers;
